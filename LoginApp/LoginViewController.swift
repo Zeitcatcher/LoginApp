6ffc687dc2311660e915acfd0932d791929241d6
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-07-31 22:01:16 -0700

    Finished

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -12,16 +12,50 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    private let userName = "user"
+    private let password = "123"
 
-    @IBAction func usernameButtonPressed() {
+    @IBAction func loginButtonPressed() {
+        guard loginTextField.text == userName, passwordTextField.text == password else {
+            errorAlert(with: "Ups", message: "Login or Password incorrect", textField: passwordTextField)
+            return
+        }
+        
+        performSegue(withIdentifier: "showWelcomeViewController", sender: nil)
     }
     
-    @IBAction func passwordButtonPressed() {
+    @IBAction func forgotLoginData(_ sender: UIButton) {
+        sender.tag == 0
+            ? errorAlert(with: "Ups", message: "Your username is: \(userName)")
+            : errorAlert(with: "Ups", message: "Your password is: \(password)")
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeViewController = segue.destination as? WelcomeViewController else { return }
+        welcomeViewController.welcomeMessage = "Welcome, \(userName)"
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
 }
 
+// MARRK: AlertController
+
+extension LoginViewController {
+    private func errorAlert(with title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            textField?.text = ""
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -11,13 +11,30 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var welcomeLabel: UILabel!
     
+    var welcomeMessage = ""
+    
+//    private let primaryColor = UIColor(
+//        red: 210/255,
+//        green: 109/255,
+//        blue: 128/255,
+//        alpha: 1
+//    )
+//
+//    private let secondaryColor = UIColor(
+//        red: 107/255,
+//        green: 148/255,
+//        blue: 230/255,
+//        alpha: 1
+//    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        welcomeLabel.text = welcomeMessage
     }
 
     @IBAction func logOutButtonPressed() {
+        view.window?.rootViewController?.dismiss(animated: true)
     }
     
 }
