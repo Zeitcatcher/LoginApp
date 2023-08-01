6680299add5850d3129c1119e64fc4877c455f30
Arseniy Oksenoyt <a.oksenoyt@gmail.com>
2023-07-31 20:31:45 -0700

    Interface setup finished

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Arseniy Oksenoyt on 7/31/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func usernameButtonPressed() {
+    }
+    
+    @IBAction func passwordButtonPressed() {
+    }
+    
+}
+
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  LoginApp
 //
 //  Created by Arseniy Oksenoyt on 7/31/23.
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func logOutButtonPressed() {
+    }
+    
 }
-
